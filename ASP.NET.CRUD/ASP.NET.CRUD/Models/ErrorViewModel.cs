5502b233ecad3739d6ef0a722cb9c60df8b65587
PankornSupercool_PC <47814115+sepankorn@users.noreply.github.com>
2021-04-08 21:29:27 +0700

    Create Structure

---
+++ ASP.NET.CRUD/ASP.NET.CRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NET.CRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
