7a3c40d280df317e7f3cfc566d85226c1dc81670
PankornSupercool_PC <47814115+sepankorn@users.noreply.github.com>
2021-04-09 03:41:39 +0700

    CSS FIX

--- ASP.NET.CRUD/ASP.NET.CRUD/Startup.cs
+++ ASP.NET.CRUD/ASP.NET.CRUD/Startup.cs
@@ -51,7 +51,7 @@ namespace ASP.NET.CRUD
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Todolist}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
