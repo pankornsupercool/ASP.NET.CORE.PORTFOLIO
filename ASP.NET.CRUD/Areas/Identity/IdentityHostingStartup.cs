68f77c0f923c8ca492cec5361edca11d4ecc9ad8
PankornSupercool_PC <47814115+sepankorn@users.noreply.github.com>
2021-04-10 02:11:52 +0700

    Complete Authentication with Todolist

---
+++ ASP.NET.CRUD/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ASP.NET.CRUD.Areas.Identity.Data;
+using ASP.NET.CRUD.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ASP.NET.CRUD.Areas.Identity.IdentityHostingStartup))]
+namespace ASP.NET.CRUD.Areas.Identity
+{ 
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options =>
+                {
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                }) 
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
